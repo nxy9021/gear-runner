13290ce803dfc12661b589c2c26337e50d2b015a
Nanxi Yu <nanxiyu@gmail.com>
2020-04-21 00:41:20 -0500

    layout functions in js

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+// clock generator
+    // determine time interval for ticks in ms; print out time (framerate) times per second
+    let frameRate = 60;
+    let frameTimeInterval = 1000/frameRate;
+    function printTime () {
+     console.log(Date.now());
+    };
+    setInterval(printTime, frameTimeInterval);
+
+// image loader
+
+// car display
+// crack display
+// background display
+
+// car control
+// spawing crack
+// track movement
+// collision detection
+// score counter
+
+// difficulty curve
+
+// car animations
+// background animations
+
+// game start
+// game end
