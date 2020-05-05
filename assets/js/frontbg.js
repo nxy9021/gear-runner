b20a9c3dcf718ebecbd2fc2622e6f04646150336
Nanxi Yu <nanxiyu@gmail.com>
2020-05-05 03:18:52 -0500

    added method to pan the image

--- assets/js/frontbg.js
+++ assets/js/frontbg.js
@@ -1,16 +1,27 @@
 class FrontBg{
     x = 0;
     y = 96;
-
+    _scrollSpeed = 2;
     _image;
     _imageLocation;
+    _currentAnimation;
+    _animation;
+    _canvasWidth;
 
     _defaultImage;
 
-    constructor(imageAssets){
+
+    constructor(imageAssets, framerate, canvasWidth){
         this._defaultImage = imageAssets.frontBg;
+        this._canvasWidth = canvasWidth;
         this._imageLocation = this._defaultImage;
+        this._setupAnimation(framerate);
         this._loadImage();
+    }
+
+    _setupAnimation(framerate){
+        this._animation = new AnimationFrame(framerate, () => this._upDate());
+        this._animation.start();
     }
 
     _loadImage(){
@@ -23,15 +34,22 @@ class FrontBg{
         this._image.src = this._imageLocation;
     }
 
+    _upDate(){
+        this.x -= this._scrollSpeed;
+        if(this.x % this._canvasWidth == 0){
+            this.x = 0; 
+        }
+    }
+
     _draw (){
         ctx.beginPath();
         ctx.drawImage(
             this._image,
             this.x,
             this.y,
-            );
+        );
     }
-
+    
     tick() {
         this._draw();
     }
