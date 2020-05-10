5d400ec2ee8beb4a3f8296249e0a34676236ed76
Nanxi Yu <nanxiyu@gmail.com>
2020-05-10 16:15:53 -0500

    created crack animation

--- assets/js/crack.js
+++ assets/js/crack.js
@@ -1,3 +1,65 @@
 class Crack {
+    x = 800;
+    y = 216;
+
+    _scrollSpeed;
+    _image;
+    _imageLocation;
+    _currentAnimation;
+    _animation;
+    _canvasWidth;
+
+    constructor(imageAssets, framerate, scrollSpeed, canvasWidth){
+        this._currentAnimation = imageAssets.style1;
+        this._scrollSpeed = scrollSpeed;
+        this._canvasWidth = canvasWidth;
+        this._imageLocation = this._currentAnimation;
+        this._setupAnimation(framerate);
+        this._loadImage();
+    }
+
+    getPosition(){
+        return {
+            xInitial: this.x,
+            yInitial: this.y,
+            xFinal: this.x + this._image.width,
+            yFinal: this.y + this._image.height
+        }
+    }
+
+    _setupAnimation(framerate){
+        this._animation = new AnimationFrame(framerate, () => this._upDate());
+        this._animation.start();
+    }
+
+    _loadImage(){
+        this._image = new Image();
+    
+        this._image.onload = () => {
+            this._draw();
+        }
+
+        this._image.src = this._currentAnimation;
+    }
+
+    _upDate(){
+        this.x -= this._scrollSpeed;
+        if(this.x < 0){
+            this.x = this._canvasWidth; 
+        }
+    }
+
+    _draw (){
+        ctx.beginPath();
+        ctx.drawImage(
+            this._image,
+            this.x,
+            this.y,
+        );
+    }
+    
+    tick() {
+        this._draw();
+    }
     
 }
